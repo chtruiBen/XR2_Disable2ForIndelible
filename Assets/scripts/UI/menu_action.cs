17a0bef150022529135d8309712eb5b059895821
chtruiBen <hsiang0418@gmail.com>
2021-05-02 15:26:29 +0800

    create file

---
+++ Assets/scripts/UI/menu_action.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+//
+public class menu_action : MonoBehaviour
+{
+	public void clickStart()
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene(1);
+    }
+    public void clickExit()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        Debug.Log("load menu_action");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/cameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{   
+    public GameObject VRcamera;
+    public GameObject target;
+    public float speed = 0.1f;
+    private Vector3 cameraPosition;
+    private Vector3 cameraDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        cameraPosition = VRcamera.transform.position;
+        cameraDirection = target.transform.position - cameraPosition;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * speed  * cameraDirection.z * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * cameraDirection.x * Time.deltaTime);
+
+        /*cameraPosition.x += cameraDirection.x * speed * Time.deltaTime * 0.1f;
+        cameraPosition.z += cameraDirection.z * speed * Time.deltaTime * 0.1f;
+        transform.Translate(cameraDirection * Time.deltaTime * Input.GetAxis("Vertical"), Space.Self);
+        */
+        //this.transform.position = cameraPosition ;
+        //Debug.Log
+
+    }
+}
